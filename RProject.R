77694ab6c9712c32ce03e59d7bb56da744bedb11
Jordan Yoder <yoderj@lafayette.edu>
2021-11-16 10:17:27 -0500

    Entry2
    Fixing Variable assignment

--- RProject.R
+++ RProject.R
@@ -1,17 +1,19 @@
 
 deal <- function(d){
   
-  hand = sample(d, 2, replace = FALSE)
-  paste(c("Hand is "), hand)
+  hand <- sample(d, 2, replace = FALSE) #choosing the 2 cards out of the deck
+  paste(c("Hand is"), hand) #printing hand
   return(d)
 }
-i = 0
+
 blackjack <- function(decks, people){
-  i = people
-  dec = c("A","A","A","A",10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,9,9,9,9,8,8,8,8,7,7,7,7,6,6,6,6,5,5,5,5,4,4,4,4,3,3,3,3,2,2,2,2)
-  for(x in 1:i)
+  i <- people
+  
+  dec <- c("A","A","A","A",10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,9,9,9,9,8,8,8,8,7,7,7,7,6,6,6,6,5,5,5,5,4,4,4,4,3,3,3,3,2,2,2,2)
+  
+  for(x in 0:i)
   {
-    dec = deal(dec)
+    dec <- deal(dec) #updating deck with new deck after cards taken
   }
   return(1)
 }
