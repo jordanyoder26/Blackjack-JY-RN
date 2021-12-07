5d7c9910be388c8b16fef522e95ef18325f99743
Jordan Yoder <yoderj@lafayette.edu>
2021-12-07 11:24:34 -0500

    Not really any changes

--- RProject.R
+++ RProject.R
@@ -1,23 +1,28 @@
 
-deal <- function(d){
-  
-  hand <- sample(d, 2, replace = FALSE) #choosing the 2 cards out of the deck
-  paste(c("Hand is"), hand) #printing hand
-  return(d)
-}
+
 
 blackjack <- function(decks, people){
   i <- people
   
   dec <- c("A","A","A","A",10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,9,9,9,9,8,8,8,8,7,7,7,7,6,6,6,6,5,5,5,5,4,4,4,4,3,3,3,3,2,2,2,2)
   
-  for(x in 0:i)
+  for(x in 1:i)
   {
     dec <- deal(dec) #updating deck with new deck after cards taken
   }
   return(1)
 }
 
-
+deal <- function(d){ #taking in deck and sending back deck after cards taken out
+  
+  index = sample(1:length(d), 2)
+  hand = d[index]
+  d = d[-index]
+  
+  print("Hand:") #printing hand
+  print(hand)
+  
+  return(d)
+}
 
   
